2fbcaea27d363dabde47e88f741a16c7990d31f6
Bard-Budist <op7puro@live.com>
2020-04-03 23:57:36 -0500

    Add task advance

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,6 @@
+# Puppet
+
+exec { 'HTTP_puppet':
+  command  => "sudo apt-get -y update && sudo apt-get -y install nginx && sudo echo 'Holberton School' | sudo tee /var/www/html/index.html && sed -i '/sendfile on;/a add_header X-Served-By $HOSTNAME;' /etc/nginx/nginx.conf && sudo service nginx restart",
+  provider => 'shell',
+}
