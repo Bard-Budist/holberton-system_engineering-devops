9606c6456b6885bbc98a5f87aa34ead8bd364367
Bard-Budist <op7puro@live.com>
2020-05-04 18:49:22 -0500

    Fix task

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,17 +9,19 @@ import sys
 
 if __name__ == "__main__":
     content = {}
+    count = 1
     number_user = requests.get(
         "https://jsonplaceholder.typicode.com/users")
-    for employer in range(1, len(number_user.json())):
+    for employer in range(0, len(number_user.json())):
         data = requests.get(
             "https://jsonplaceholder.typicode.com/todos",
             params={'userId': number_user.json()[employer]['id']}).json()
-        content[str(employer)] = [{
+        content[str(count)] = [{
             'username': number_user.json()[employer]['username'],
             'task': itemp['title'],
             'completed': itemp['completed']
         } for itemp in data]
+        count += 1
     file_json = open("todo_all_employees.json", mode="w+")
     json.dump(content, file_json)
     file_json.close()
