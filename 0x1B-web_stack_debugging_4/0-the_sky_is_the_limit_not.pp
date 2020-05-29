fbe1b8ea712bcace11fb6c226f0808afcfcfcc91
Bard Budist <44384347+Bard-Budist@users.noreply.github.com>
2020-05-28 23:27:58 -0500

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,13 @@
+#Puppet
 file { '/etc/default/nginx':
     ensure  => 'present',
     replace => 'yes',
     content => 'ULIMIT="-n 4096"',
     mode    => '0644',
+    before => Exec['restart'],
   }
+
+exec { 'restart':
+     provider => 'shell',
+     command  => "sudo service nginx restart",
+}
